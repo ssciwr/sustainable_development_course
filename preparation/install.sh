9896758b53a95f05c8c2f405711cc617d74f50db
iulusoy <inga.ulusoy@uni-heidelberg.de>
2021-03-02 14:16:14 +0100

    fixed pip command

--- preparation/install.sh
+++ preparation/install.sh
@@ -2,7 +2,7 @@
 # Installation script for required libraries
 # ISU 03/21
 sudo apt install python3.8 python3-pip python3-tk
-pip install numpy pandas seaborn jupyter
+pip3 install numpy pandas seaborn jupyter
 echo "export PATH=\$PATH:$HOME/.local/bin/" >> ~/.bashrc
 echo "export DISPLAY=localhost:0.0" >> ~/.bashrc
 source ~/.bashrc
