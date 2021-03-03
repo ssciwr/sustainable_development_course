f5504651f7029ced17141c982e78b1e927915be2
iulusoy <inga.ulusoy@uni-heidelberg.de>
2021-03-03 08:52:12 +0100

    install script less invasive

--- preparation/install.sh
+++ preparation/install.sh
@@ -4,5 +4,5 @@
 sudo apt install python3.8 python3-pip python3-tk
 pip3 install numpy pandas seaborn jupyter
 echo "export PATH=\$PATH:$HOME/.local/bin/" >> ~/.bashrc
-echo "export DISPLAY=localhost:0.0" >> ~/.bashrc
+export DISPLAY=localhost:0.0
 source ~/.bashrc
