7a95fd4810d5c1b7a51899ceb905285360f4e5d7
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2023-02-20 13:58:30 +0100

    more updates on unit 1

--- preparation/install.sh
+++ preparation/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Installation script for required libraries
 # ISU 03/21
-sudo apt install python3.8 python3-pip python3-tk
+sudo apt install python3.9 python3-pip python3-tk
 pip3 install numpy pandas seaborn jupyter
 echo "export PATH=\$PATH:$HOME/.local/bin/" >> ~/.bashrc
 export DISPLAY=localhost:0.0
